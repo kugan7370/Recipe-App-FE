71abc5ad0fba4c2e714f4d92e61e37a34558d171
Kugan <kugapriyanj97@gmail.com>
2024-10-21 22:49:05 +0530

    initialized structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
 
 function App() {
-
   return (
-        <h1 className="text-3xl font-bold underline text-red-400">
+        <h1 className="text-3xl underline text-red-400 font-poppins ">
           Hello world!
         </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export const content = [
   "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
 ];
 export const theme = {
+  fontFamily: {
+    poppins: ['Poppins', 'sans-serif'],
+    'poppins-medium': ['Poppins-Medium', 'sans-serif'],
+    'poppins-semibold': ['Poppins-SemiBold', 'sans-serif'],
+    'poppins-bold': ['Poppins-Bold', 'sans-serif'],
+  },
   extend: {},
 };
 export const plugins = [];
