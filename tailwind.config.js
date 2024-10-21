656314d6707908bd01096f3c023fe7cd9ba6d2e5
Kugan <kugapriyanj97@gmail.com>
2024-10-21 22:26:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
